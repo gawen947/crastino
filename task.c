804f13278a0871f201eb3f34875fa74eb4f57b84
David Hauweele <david@hauweele.net>
2013-01-23 18:27:32 +0100

    Adds task functions and structures.
    The task fuctions and structures are used to manipulate tasks.

---
+++ task.c
@@ -0,0 +1,52 @@
+/* File: task.c
+   Time-stamp: <2013-01-23 18:21:36 gawen>
+
+   Copyright (C) 2013 David Hauweele <david@hauweele.net>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
+#include <stdbool.h>
+#include <time.h>
+#include <assert.h>
+
+#include "common.h"
+#include "task.h"
+
+struct task * creat_task(const char *title, const char *message, bool recursive)
+{
+  struct task *task = xmalloc(sizeof(struct task));
+  time_t now = time(NULL);
+  task->ctime = task->mtime = task->dtime = now;
+  task->title     = title;
+  task->message   = message;
+  task->recursive = recursive;
+
+  return task;
+}
+
+void modify_task(struct task *task, const char *title, const char *message)
+{
+  task->title   = title;
+  task->message = message;
+  task->mtime   = time(NULL);
+}
+
+void mark_as_done(struct task *task)
+{
+  /* The task should be recursive. Non-recursive task are
+     deleted when marked as done. */
+  assert(task->recursive);
+
+  task->dtime = time(NULL);
+}
---
+++ task.h
@@ -0,0 +1,50 @@
+/* File: task.h
+   Time-stamp: <2013-01-23 18:26:45 gawen>
+
+   Copyright (C) 2013 David Hauweele <david@hauweele.net>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef _TASK_H_
+#define _TASK_H_
+
+#include <stdbool.h>
+#include <time.h>
+
+/* This structure represents a task in its simpliest form. */
+struct task {
+  const char *title;   /* short description of the task */
+  const char *message; /* full description of the task */
+
+  time_t ctime;        /* date of creation */
+  time_t mtime;        /* date of last modification */
+  time_t dtime;        /* last date this task was marked as done */
+
+  bool recursive;      /* marked as a recursive task */
+};
+
+/* Create a task structure with propely time. The structure has to be freed
+   manually. The passed should be freed manually too to avoid useless string
+   duplication. */
+struct task * creat_task(const char *title, const char *message,
+                         bool recursive);
+
+/* Modify a task and setup the time accordingly. */
+void modify_task(struct task *task, const char *title, const char *message);
+
+/* Mark the task as done and setup the time accordingly.
+   The task should be a recursive task. */
+void mark_as_done(struct task *task);
+
+#endif
